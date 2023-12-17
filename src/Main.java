b40ef578aded36a4afb7d93367f9ee3de70b2544
Denyk26 <denis.maks@mail.ru>
2023-12-17 19:27:58 +0100

    Made class Main and class CustomStringList

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] colors = {"Red","Yellow","Black"};
+        CustomStringList customStringList = new CustomStringList(colors);
+
+        for (String s : customStringList.list){
+            System.out.println(s);
+        }
+    }
+}

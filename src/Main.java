ecab868bf809cd19f77a3e6344812029fb99f204
Denyk26 <denis.maks@mail.ru>
2023-12-18 15:40:33 +0100

    Refactor code

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String[] colors = {"Red", "Yellow", "Black"};
-        CustomStringList customStringList = new CustomStringList(colors);
-
-        for (String s : customStringList.list) {
-            System.out.println(s);
-        }
-        System.out.println(customStringList.check("Black"));
+        CustomStringList list = new CustomStringList(new String[]{"Red", "Yellow", "Black"});
+        System.out.println(list);
+        System.out.println(list.check("Black"));
     }
 }
